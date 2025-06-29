c43b76a5965fbc34cda5a81ce780d62b7a7cad01
nascjoao <oi@nasc.dev>
2025-06-29 20:52:06 -0300

    feat: add entrypoint script for FENCE action

---
+++ .github/actions/fence/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+export FENCE_LIMIT="${INPUT_LIMIT:-250}"
+export FENCE_SUCCESS="${INPUT_SUCCESS_MSG}"
+export FENCE_FAIL="${INPUT_FAIL_MSG}"
+
+BASE_BRANCH="${INPUT_BASE_BRANCH:-main}"
+
+fence "$BASE_BRANCH"
