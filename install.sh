d7a2e618e9b209c59f66e8f5a9bd727bf078495b
Wisley Alves <wisley@licitardigital.com.br>
2025-07-03 11:13:52 -0300

    fix: update installation to add libs

--- install.sh
+++ install.sh
@@ -7,16 +7,40 @@ REPO_URL="https://raw.githubusercontent.com/wisley7l/fence/main"
 SCRIPT_URL="$REPO_URL/bin/fence.sh"
 FENCE_DIR="/usr/local/bin/.fence"
 
+echo "FENCE_DIR: $FENCE_DIR"
+echo "REPO_URL: $REPO_URL"
+echo "SCRIPT_URL: $SCRIPT_URL"
+
+download_files() {
+  local isSudo=$1
+  local sudo=""
+  
+  if [ "$isSudo" = "true" ]; then
+    sudo="sudo"
+  fi
+  
+  $sudo curl -sSL "$SCRIPT_URL" -o "$INSTALL_PATH" && $sudo chmod +x "$INSTALL_PATH"
+  
+  $sudo mkdir -p "$FENCE_DIR"
+  if [ ! -d "$FENCE_DIR" ]; then
+    echo "❌ Erro: Falha ao criar o diretório $FENCE_DIR"
+    echo "   Verifique as permissões e tente novamente."
+    exit 1
+  fi
+  
+  $sudo chmod 755 "$FENCE_DIR"
+  $sudo curl -sSL "$REPO_URL/lib/common.sh" -o "$FENCE_DIR/common.sh"
+  $sudo curl -sSL "$REPO_URL/lib/help.sh" -o "$FENCE_DIR/help.sh"
+  $sudo curl -sSL "$REPO_URL/lib/remote.sh" -o "$FENCE_DIR/remote.sh"
+  $sudo curl -sSL "$REPO_URL/lib/local.sh" -o "$FENCE_DIR/local.sh"
+}
+
 if [ "$(id -u)" -ne 0 ]; then
   echo "⚠️  It requires administrator permissions to install it in $INSTALL_PATH."
   echo "The command will run with sudo."
-  sudo curl -sSL "$SCRIPT_URL" -o "$INSTALL_PATH" && sudo chmod +x "$INSTALL_PATH"
-  sudo wget -P "$FENCE_DIR" -q "$REPO_URL/local/common.sh"
-  sudo wget -P "$FENCE_DIR" -q "$REPO_URL/local/help.sh"
-  sudo wget -P "$FENCE_DIR" -q "$REPO_URL/local/remote.sh"
-  sudo wget -P "$FENCE_DIR" -q "$REPO_URL/local/local.sh"
+  download_files true
 else
-  curl -sSL "$SCRIPT_URL" -o "$INSTALL_PATH" && chmod +x "$INSTALL_PATH"
+  download_files false
 fi
 
 if [ $? -eq 0 ]; then
