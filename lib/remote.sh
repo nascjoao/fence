7dbae9aa3aaf644e8d5f077045569a62182f0807
nascjoao <oi@nasc.dev>
2025-07-03 08:11:23 -0300

    feat: add file for remote handling functions

---
+++ lib/remote.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+has_remote() {
+  git remote get-url origin >/dev/null 2>&1
+}
+
+fetch_remote_branch() {
+  git fetch origin "$1" >/dev/null 2>&1
+}
+
+use_remote_mode() {
+  BRANCH=${BASE_BRANCH:-main}
+
+  has_remote || error "No remote 'origin' found."
+  fetch_remote_branch "$BRANCH" || error "Could not fetch origin/$BRANCH."
+  DIFF_BASE="origin/$BRANCH"
+}
